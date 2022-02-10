7439dcd773f6a432ed05e95e6bdb47a77e2c8cb0
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-02-10 20:06:20 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let num:number[]= new Array(7);
+
+num[0]=24;
+num[1]=14;
+num[2]=8;
+num[3]=0;
+num[4]=5;
+num[5]=19;
+num[6]=24;
+
+for (let index = 0; index <= num.length; index++) {
+ console.log(num[index]);
+  
+}
